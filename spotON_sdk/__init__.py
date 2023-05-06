26700a271f5553d5dcc5c41101fbf9d9e104b391
pleberer <78508395+pleberer@users.noreply.github.com>
2023-05-07 01:33:41 +0200

    add geography

--- spotON_sdk/__init__.py
+++ spotON_sdk/__init__.py
@@ -6,8 +6,10 @@ and processing responses. It also includes utility functions for working with
 the data returned by the API.
 """
 
+from entsoe import Area
 
+from .constants.geography import *
 from .logic.Config import *
 
-__version__ = "0.0.1.1"
+__version__ = "0.0.1.5"
 __all__ = [name for name in dir() if not name.startswith('_')]
